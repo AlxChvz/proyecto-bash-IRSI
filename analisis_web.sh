fd7a93f1719b7af358d557a1364d51f6dab63e97
andresdlRoca <andresdelaroca11@gmail.com>
2024-06-28 10:12:21 -0600

    Menu de opciones con case

--- analisis_web.sh
+++ analisis_web.sh
@@ -61,34 +61,62 @@ obtener_resultados_urlscan() {
     done
 }
 
-# Mostrar todos los sitios web
-echo "Mostrando todos los sitios web"
-mostrar_sitios
-read -p "Presiona Enter para continuar"
-clear
+# Función para mostrar el menú de opciones
+mostrar_menu() {
+    echo "Menú de opciones:"
+    echo "1. Mostrar sitios web"
+    echo "2. Analizar sitios web con wafw00f"
+    echo "3. Analizar puertos abiertos con nmap"
+    echo "4. Enviar URLs a URLScan.io"
+    echo "5. Obtener resultados de URLScan.io"
+    echo "6. Salir"
+    read -p "Elige una opción (1-6): " opcion
+    case $opcion in
+        1)
+            clear
+            mostrar_sitios
+            read -p "Presiona Enter para volver al menú principal"
+            clear
+            mostrar_menu
+            ;;
+        2)
+            clear
+            analizar_con_wafw00f
+            read -p "Presiona Enter para volver al menú principal"
+            clear
+            mostrar_menu
+            ;;
+        3)
+            clear
+            analizar_con_nmap
+            read -p "Presiona Enter para volver al menú principal"
+            clear
+            mostrar_menu
+            ;;
+        4)
+            clear
+            enviar_a_urlscan
+            read -p "Presiona Enter para volver al menú principal"
+            clear
+            mostrar_menu
+            ;;
+        5)
+            clear
+            obtener_resultados_urlscan
+            read -p "Presiona Enter para volver al menú principal"
+            clear
+            mostrar_menu
+            ;;
+        6)
+            echo "Saliendo del script..."
+            exit 0
+            ;;
+        *)
+            echo "Opción no válida. Por favor, elige una opción del 1 al 6."
+            mostrar_menu
+            ;;
+    esac
+}
 
-# Usar bucle for para analizar los sitios web con wafw00f
-echo "Usando el ciclo for para analizar con wafw00f"
-analizar_con_wafw00f
-read -p "Presiona Enter para continuar"
-clear
-
-# Usar bucle for para analizar puertos abiertos con nmap
-echo "Usando el ciclo for para analizar puertos con nmap"
-analizar_con_nmap
-read -p "Presiona Enter para continuar"
-clear
-
-# Enviar URLs a URLScan.io
-echo "Enviando URLs a URLScan.io"
-enviar_a_urlscan
-read -p "Presiona Enter para continuar"
-clear
-
-# Obtener resultados de URLScan.io
-echo "Obteniendo resultados de URLScan.io"
-obtener_resultados_urlscan
-read -p "Presiona Enter para finalizar"
-clear
-
-echo "Script finalizado."
+# Iniciar el script mostrando el menú principal
+mostrar_menu
